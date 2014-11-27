1f839d9658139f07a1404102018d35b5443b4bcc
Cor-Paul Bezemer <c.bezemer@tudelft.nl>
2014-11-27 11:53:50 +0100

    dfg stuff

--- graphs/generate_dfg_report.sh
+++ graphs/generate_dfg_report.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -xe
 
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 if [ $# -ne $EXPECTED_ARGS ]
 then
-	echo "Usage: `basename $0` v1.svg v2.svg diff.svg"
+	echo "Usage: `basename $0` v1.svg v2.svg diff.svg outputdir"
+	echo "Note: the svg files should be in the same directory as the generated dfg-set.html file."
 	exit 65
 fi
 
-cp graphs/dfg_template.html output/dfg.html
-sed -i "s/%V1FILE%/$1/g" output/dfg.html 
-sed -i "s/%V2FILE%/$2/g" output/dfg.html 
-sed -i "s/%DIFFFILE%/$3/g" output/dfg.html 
+OUTPUTDIR=$4
+
+cp graphs/dfg_template.html $OUTPUTDIR/dfg-set.html
+sed -i "s|%V1FILE%|$1|g" $OUTPUTDIR/dfg-set.html
+sed -i "s|%V2FILE%|$2|g" $OUTPUTDIR/dfg-set.html
+sed -i "s|%DIFFFILE%|$3|g" $OUTPUTDIR/dfg-set.html
